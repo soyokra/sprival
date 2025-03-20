43a88fed9980f737529a7010f1da935651734528
zhengchenping <zhengchenping@zuzuche.com>
2025-03-20 15:13:55 +0800

    +

--- src/main/java/com/soyokra/sprival/support/ck/ClickHouseAutoConfiguration.java
+++ src/main/java/com/soyokra/sprival/support/ck/ClickHouseAutoConfiguration.java
@@ -1,19 +1,5 @@
 package com.soyokra.sprival.support.ck;
 
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitOperations;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
-import org.springframework.boot.autoconfigure.amqp.RabbitRetryTemplateCustomizer;
-import org.springframework.boot.autoconfigure.amqp.RabbitTemplateConfigurer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +28,5 @@ public class ClickHouseAutoConfiguration {
         public ClickHouseTemplate clickHouseTemplate(ClickHouseConnectionFactory connectionFactory) {
             return new ClickHouseTemplate(connectionFactory);
         }
-
     }
 }
