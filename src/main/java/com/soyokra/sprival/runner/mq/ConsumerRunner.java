8f26fb3e4458d972c354be87cfa80aa384fd39a6
zhengchenping <zhengchenping@zuzuche.com>
2025-07-04 15:18:08 +0800

    +

--- src/main/java/com/soyokra/sprival/runner/mq/ConsumerRunner.java
+++ src/main/java/com/soyokra/sprival/runner/mq/ConsumerRunner.java
@@ -24,27 +24,31 @@ public class ConsumerRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
 
-        IAdmin adminImpl = mqFactory.admin(MqFactory.Driver.RABBITMQ);
-        IConsumer consumerImpl = mqFactory.consumer(MqFactory.Driver.RABBITMQ);
+        try {
+            IAdmin adminImpl = mqFactory.admin(MqFactory.Driver.RABBITMQ);
+            IConsumer consumerImpl = mqFactory.consumer(MqFactory.Driver.RABBITMQ);
 
-        List<Queue> queueList = new ArrayList<>();
-        queueList.add(Queue.valueOf("test_a"));
-        queueList.add(Queue.valueOf("test_b"));
-        queueList.add(Queue.valueOf("test_c"));
-        queueList.add(Queue.valueOf("test_delay_5", 5));
-        queueList.add(Queue.valueOf("test_delay_10", 10));
-        queueList.add(Queue.valueOf("test_delay_30", 30));
-        for (Queue queue : queueList) {
-            adminImpl.declareQueue(queue);
+            List<Queue> queueList = new ArrayList<>();
+            queueList.add(Queue.valueOf("test_a"));
+            queueList.add(Queue.valueOf("test_b"));
+            queueList.add(Queue.valueOf("test_c"));
+            queueList.add(Queue.valueOf("test_delay_5", 5));
+            queueList.add(Queue.valueOf("test_delay_10", 10));
+            queueList.add(Queue.valueOf("test_delay_30", 30));
+            for (Queue queue : queueList) {
+                adminImpl.declareQueue(queue);
+            }
+
+            List<Consumer> consumerList = new ArrayList<>();
+            consumerList.add(Consumer.valueOf("test_a", Queue.valueOf("test_a")));
+            consumerList.add(Consumer.valueOf("test_b", Queue.valueOf("test_b")));
+            consumerList.add(Consumer.valueOf("test_c", Queue.valueOf("test_c")));
+            consumerList.add(Consumer.valueOf("test_delay_5", Queue.valueOf("test_delay_5")));
+            consumerList.add(Consumer.valueOf("test_delay_10", Queue.valueOf("test_delay_10")));
+            consumerList.add(Consumer.valueOf("test_delay_30", Queue.valueOf("test_delay_30")));
+            consumerImpl.listenerUp(consumerList);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
-
-        List<Consumer> consumerList = new ArrayList<>();
-        consumerList.add(Consumer.valueOf("test_a", Queue.valueOf("test_a")));
-        consumerList.add(Consumer.valueOf("test_b", Queue.valueOf("test_b")));
-        consumerList.add(Consumer.valueOf("test_c", Queue.valueOf("test_c")));
-        consumerList.add(Consumer.valueOf("test_delay_5", Queue.valueOf("test_delay_5")));
-        consumerList.add(Consumer.valueOf("test_delay_10", Queue.valueOf("test_delay_10")));
-        consumerList.add(Consumer.valueOf("test_delay_30", Queue.valueOf("test_delay_30")));
-        consumerImpl.listenerUp(consumerList);
     }
 }
