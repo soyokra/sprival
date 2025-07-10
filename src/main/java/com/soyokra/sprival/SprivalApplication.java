95f0777035d5f3ec4900aae1708880d1b2336a98
zhengchenping <zhengchenping@zuzuche.com>
2025-07-10 21:12:32 +0800

    +

--- src/main/java/com/soyokra/sprival/SprivalApplication.java
+++ src/main/java/com/soyokra/sprival/SprivalApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableCaching
 @MapperScan("com.soyokra.sprival.dao.*.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
 @EnableAsync(proxyTargetClass = true)
