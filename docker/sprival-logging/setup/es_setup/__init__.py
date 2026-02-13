c8c20e2b810055d84272e5f78582cdf96ab9982a
soyokra <soyokra@163.com>
2026-02-13 17:55:50 +0800

    +

--- docker/sprival-logging/setup/es_setup/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Elasticsearch setup module for initializing users, roles, and index templates.
-"""
-
-__version__ = "1.0.0"
-
--- docker/sprival-monitoring/setup/grafana_setup/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Grafana 配置模块。
-
-提供 Grafana 自动配置功能，包括数据源管理和 Dashboard 导入。
-"""
-
